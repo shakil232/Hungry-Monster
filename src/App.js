023f73f041d94531191f22ed65917323c0c9bc78
shakil232 <dxshakil2@gmail.com>
2022-06-27 23:52:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+function App() {
+//   <Router>
+//     <Routes>
+//       <Route path="/about" />
+//       </Routes>
+// </Router>
+
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faCopy, faQuestionCircle, faQrcode, faGithub } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// <FontAwesomeIcon icon={faCoffee} />
+
+  return (
+    <div >
+     
+    </div>
+  );
+}
+
+
+export default App;
