65786bb2f146f529a709201d0dffd4d60028a1b1
shakil232 <dxshakil2@gmail.com>
2022-06-30 06:01:03 +0600

    netlify not found error handel

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -15,8 +15,11 @@ const About = () => {
                         <h2 className="text-muted"> Hungry-Monster
                         </h2>
                         <p className="mt-4 text-muted text-justify">
-                            Urban-Rider is a website of ride share online. The website is built with React, Firebase, React-Router, and React-Bootstrap. And has been used Authentication of Firebase .
-                            <br /> If the user clicks on the vehicle icon, the user will be taken to the login page for Sign up or Sign in to view the destination page. Then the user must be Sign up and log in to view the destination page
+                            Hungry-Monster is a single-page online food order website. it is a simple react-router application. The website dynamically changes the route with the URL. <br />
+                            The website is built with React, React-Router and React-Bootstrap. <br />
+                            There is a Homepage. The homepage has a search input field and displays food items, a button named add to cart and a Link name learn more.  <br />
+                            If you click on the add to cart button, you will be adding the meal to your cart and you click on the learn more Link taken to the next page to view Meal detail.
+
                         </p>
 
                     </div>
